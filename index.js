ecf317ef652b4b46b9b0ed2196b5f9d13be67071
ew-kislov <evgeniykislov12@gmail.com>
2025-11-09 22:13:42 +0100

    Added more headers to test request

--- index.js
+++ index.js
@@ -48,7 +48,11 @@ async function getReviewsPages(country, hotel, options = {}) {
 
   let sampleBooking = await fetch('https://booking.com', {
     headers: {
-      'Cookie': startingCookie
+      'Cookie': startingCookie,
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/... Chrome/..',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Referer': 'https://www.booking.com/'
     }
   });
   console.log('Sample http BOOKING request status - ' + sampleBooking.status)
